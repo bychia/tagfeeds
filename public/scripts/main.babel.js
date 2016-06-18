989671eedad2a2ef40af9a28d0518914220cfd4c
ChrisChia <chrischia82@gmail.com>
2016-06-18 22:37:57 +0800

    restoring back the default page after fixing the incognito bug (cors issues)

--- public/scripts/main.babel.js
+++ public/scripts/main.babel.js
@@ -53,7 +53,7 @@ var TableBox = React.createClass({
         this.setState({ data: data });
       }.bind(this),
       error: function (xhr, status, err) {
-        console.error(this.props.url, status, err.toString());
+        console.log(this.props.url, status, err.toString());
       }.bind(this)
     });
   },
