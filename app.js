20e1203b0c48d5398e5181cb89b2db967cc89c25
ChrisChia <chrischia82@gmail.com>
2017-02-04 22:37:12 +0800

    update most url reference to asset.tagfeeds.com/.

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const path = require('path');
+const app = express();
 
 app.use(express.static('public'))
+
+app.get('*', function(req, res){
+    res.sendFile(path.join(__dirname, './public', 'index.html'));
+});
+
 app.listen(3000);
