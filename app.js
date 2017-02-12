f317bfaee4601037d766c112e8b48e54ef9a9218
ChrisChia <chrischia82@gmail.com>
2017-02-12 13:24:38 +0800

    absolute path for the javascript and css assets

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(express.static('public'))
 
 app.get('/', function(req, res){
